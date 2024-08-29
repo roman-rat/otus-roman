e0313bf0419ec7a23e2a97351bca1f1e1c82260c
roman-rat <atrashyonok2013roma@gmail.com>
2024-08-29 18:28:39 +0300

    fix

--- Lession 11/test.sh
+++ Lession 11/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Настройки
-LOG_FILE="/var/log/messages"          # файл логов системных сообщений
-EMAIL="atrashyonok2014@gmail.com"           # email для отправки отчета
+LOG_DIR="/var/log/nginx/"           
+EMAIL="atrashyonok2014@gmail.com"    
 LOCKFILE="/tmp/send_report.lock"      # файл блокировки для предотвращения одновременного запуска
 LAST_RUN_FILE="/tmp/last_run_time.txt" # файл с временной меткой последнего запуска
-CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S") # текущее время
+CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S") 
 TEMP_LOG="/tmp/temp_log.log"          # временный файл для хранения логов
 TEMP_REPORT="/tmp/report.txt"         # временный файл для хранения отчета
 
 # Установка одной ловушки (trap) для очистки ресурсов при завершении или ошибке
 trap 'cleanup_and_exit' INT TERM EXIT
 
-# Функция очистки ресурсов
+# Функция очистки ресов
 cleanup_and_exit() {
   rm -f "$TEMP_LOG" "$TEMP_REPORT"
   rm -f "$LOCKFILE"
@@ -40,32 +40,34 @@ determine_time_range() {
 
 # Функция загрузки логов
 load_logs() {
-  # Используем awk для выбора строк по временной метке
-  awk -v last_run="$LAST_RUN_TIME" '$0 > last_run' "$LOG_FILE" > "$TEMP_LOG"
+  find "$LOG_DIR" -type f -name "*.log" -newermt "$LAST_RUN_TIME" -exec cat {} \; > "$TEMP_LOG"
+  sed -i 's/[\[\]"]//g' "$TEMP_LOG"
 }
 
 # Функция анализа логов
 analyze_logs() {
-  # Анализ данных по системному логу /var/log/messages
-  TOP_SERVICES=$(awk '{print $5}' "$TEMP_LOG" | sort | uniq -c | sort -nr | head -n 10)
-  ERRORS=$(grep -i "error" "$TEMP_LOG")
-  WARNINGS=$(grep -i "warning" "$TEMP_LOG")
+  TOP_IPS=$(awk '{print $1}' "$TEMP_LOG" | sort | uniq -c | sort -nr | head -n 10) #Извлекает IP-адреса из логов
+  TOP_URLS=$(awk '{print $7}' "$TEMP_LOG" | sort | uniq -c | sort -nr | head -n 10) #Извлекает URL из логов
+  ERRORS=$(grep "error" "$TEMP_LOG") #Ищет все строки, содержащие "error"
+  HTTP_CODES=$(awk '{print $9}' "$TEMP_LOG" | grep -E '^[0-9]{3}$' | sort | uniq -c | sort -nr) #Извлекает коды HTTP-ответов
 }
 
 # Функция создания отчета
 create_report() {
   echo "Отчет за период: $LAST_RUN_TIME - $CURRENT_TIME" > "$TEMP_REPORT"
-  echo -e "\nСписок служб с наибольшим количеством сообщений:" >> "$TEMP_REPORT"
-  echo "$TOP_SERVICES" >> "$TEMP_REPORT"
-  echo -e "\nОшибки:" >> "$TEMP_REPORT"
+  echo -e "\nСписок IP адресов с наибольшим количеством запросов:" >> "$TEMP_REPORT"
+  echo "$TOP_IPS" >> "$TEMP_REPORT"
+  echo -e "\nСписок URL с наибольшим количеством запросов:" >> "$TEMP_REPORT"
+  echo "$TOP_URLS" >> "$TEMP_REPORT"
+  echo -e "\nОшибки веб-сервера/приложения:" >> "$TEMP_REPORT"
   echo "$ERRORS" >> "$TEMP_REPORT"
-  echo -e "\nПредупреждения:" >> "$TEMP_REPORT"
-  echo "$WARNINGS" >> "$TEMP_REPORT"
+  echo -e "\nСписок всех кодов HTTP ответа:" >> "$TEMP_REPORT"
+  echo "$HTTP_CODES" >> "$TEMP_REPORT"
 }
 
 # Функция отправки отчета по электронной почте
 send_report() {
-  mail -s "Отчет по системным логам" "$EMAIL" < "$TEMP_REPORT"
+  mail -s "Отчет по логам веб-сервера" "$EMAIL" < "$TEMP_REPORT"
 }
 
 # Основная функция запуска скрипта
