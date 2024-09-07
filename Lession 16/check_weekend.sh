43d7de311d923ee6feaa8ea481a74e85c17560d7
roman-rat <atrashyonok2013roma@gmail.com>
2024-09-07 04:10:10 +0300

    fix

--- Lession 16/check_weekend.sh
+++ Lession 16/check_weekend.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-    # Получаем текущий день недели (1 = Понедельник, ..., 7 = Воскресенье)
-    day_of_week=$(date +%u)
 
-    # Если день суббота (6) или воскресенье (7), проверяем группу
-    if [[ "$day_of_week" -gt 5 ]]; then
-      # Проверяем, если пользователь в группе admin
-      if groups $PAM_USER | grep -q "\\badmin\\b"; then
-        exit 0
-      else
-        exit 1
-      fi
-    else
-      exit 0
-    fi'
+# Получаем текущий день недели (1 - Понедельник, 7 - Воскресенье)
+current_day=$(date +%u)
+
+# Проверяем, если день недели суббота (6) или воскресенье (7)
+if [[ $current_day -gt 5 ]]; then
+  # Проверяем, если пользователь не в группе admin
+  if ! groups $PAM_USER | grep -qw admin; then
+    exit 1
+  fi
+fi
+
+# Разрешаем доступ
+exit 0
